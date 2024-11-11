0ebadacdbb7b6bf79fcfd4e208c08c1ae1aa845e
unmetro <emlome22@gmail.com>
2024-11-10 22:38:25 -0600

    hola

--- Desarollo_de_Aplicaciones_Web/entorno-django/practica4/practica4/urls.py
+++ Desarollo_de_Aplicaciones_Web/entorno-django/practica4/practica4/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Renata.views import IndexView
+from Renata.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
